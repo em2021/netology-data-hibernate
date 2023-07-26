4596d9e7b01c31411f3e9a80ecf24db628fa5fae
em2021 <egor_m@icloud.com>
2023-07-26 12:58:55 +0300

    Updated PersonRepositoryCustom methods with @Query annotation and JPQL queries

--- src/main/java/ru/netology/hibernate/repository/PersonRepositoryCustom.java
+++ src/main/java/ru/netology/hibernate/repository/PersonRepositoryCustom.java
@@ -1,14 +1,19 @@
 package ru.netology.hibernate.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.netology.hibernate.entity.Person;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepositoryCustom {
-    Optional<List<Person>> findByCity(String city);
+    @Query(("select p from Person p where p.city = :city"))
+    Optional<List<Person>> findByCity(@Param("city") String city);
 
-    Optional<List<Person>> findByAgeLessThanOrderByAgeDesc(Integer age);
+    @Query(("select p from Person p where p.age < :age"))
+    Optional<List<Person>> findByAgeLessThanOrderByAgeDesc(@Param("age") Integer age);
 
-    Optional<Person> findByNameAndSurname(String name, String surname);
+    @Query(("select p from Person p where p.name = :name and p.surname = :surname"))
+    Optional<Person> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
